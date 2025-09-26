28b05505d44c9df302fb6db3269ecdbf092f0859
MadStoki <danni.meyer.10@gmail.com>
2025-09-25 22:38:37 -0700

    Lab1

---
+++ Lab1.py
@@ -0,0 +1,11 @@
+#******************************************************************************
+# Author:           Jordan Meyer
+# Lab:              Lab 1
+# Date:             September 25, 2025
+# Description:      This program prints "Hello World".
+# Input:            None
+# Output:           Sting: Hello World
+# Sources:          Lab 1 specifications.
+#******************************************************************************
+
+print("Hello World")
